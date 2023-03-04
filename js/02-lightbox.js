e2b525f4239ed55fb6065c15a0512e4d94da2748
Oakmoater <dubrovecva@gmail.com>
2023-03-04 20:08:36 +0100

    la fin

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryContainer = document.querySelector('.gallery');
+
 const galleryCreator = galleryItems.map(({ preview, original, description }) =>
     `<a class="gallery__item" href="${original}">
         <img
@@ -10,5 +11,11 @@ const galleryCreator = galleryItems.map(({ preview, original, description }) =>
         />
     </a>`
 ).join('');
+
 galleryContainer.insertAdjacentHTML('beforeend', galleryCreator);
 
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+}); 
