2e92e8ff7c3bad5739f78946b6ec05c33035351a
Oakmoater <dubrovecva@gmail.com>
2023-03-05 12:44:21 +0100

    hw finalization

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,6 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
-
 const galleryCreator = galleryItems.map(({ preview, original, description }) =>
     `<div class="gallery__item">
         <a class="gallery__link" href="${original}">
@@ -15,27 +14,21 @@ const galleryCreator = galleryItems.map(({ preview, original, description }) =>
         </a>
     </div>`
 ).join('');
-
 galleryContainer.insertAdjacentHTML('beforeend', galleryCreator);
 
-function openModal(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-  const imageSrc = event.target.dataset.source;
-    const modalWindow = basicLightbox.create(`
-    <img src="${imageSrc}" width="800" height="600">
-  `);
-  modalWindow.show();
-};
-galleryContainer.addEventListener('click', openModal);
 
-function closeModal(event) {
-  if (event.code === 'Escape') {
-      const modalWindow = document.querySelector('.basicLightbox');
-    //   modalWindow.close();
-      modalWindow.parentNode.removeChild(modalWindow);
-  }
-};
-document.addEventListener('keydown', closeModal);
+galleryContainer.addEventListener('click', onLinkClick);
+
+const modalWindow = basicLightbox.create(`<img width="1140" height="720" src="#">`, {onShow: (modalWindow) => {window.addEventListener('keydown', onEscPress)},
+onClose: (modalWindow) => {window.removeEventListener('keydown', onEscPress)}});
+
+function onLinkClick(event) {
+  event.preventDefault();
+  modalWindow.element().querySelector('img').src = event.target.dataset.source;
+  modalWindow.show(); 
+}
+function onEscPress(event) {
+ if(event.code === "Escape") {
+  modalWindow.close();
+ }
+}
